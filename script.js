a1b835beb245b7cdd4d5bd5ffbde9312d003f5ad
Leila <leiila.hort@gmail.com>
2024-02-23 15:50:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+<lottie-player id="eleventhLottie" src="https://assets10.lottiefiles.com/packages/lf20_gaj4x0te.json" style="width:400px; height: 400px;">"></lottie-player>
+
+<script>
+LottieInteractivity.create({
+    player:'#eleventhLottie',
+    mode:"cursor",
+    actions: [
+        {
+            type: "hover",
+            forceFlag: false
+        }
+    ]
+});
+</script>
